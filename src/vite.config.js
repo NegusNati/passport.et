bcfd390054b06b22e05b7d16931710707784c7e3
Negus <natnaelbirhanu22@gmail.com>
2024-09-06 10:29:29 +0300

    jsx

--- src/vite.config.js
+++ src/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         laravel({
             input:  [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/app.jsx',
             ],
             refresh: true,
         }),
