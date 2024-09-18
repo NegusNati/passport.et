4aed4c1c3639fc06b5a88d44fd799b830b147fbc
Negus <natnaelbirhanu22@gmail.com>
2024-09-18 11:42:56 +0300

    button gradient

--- src/resources/js/Pages/Welcome.jsx
+++ src/resources/js/Pages/Welcome.jsx
@@ -105,7 +105,7 @@ function HeroSection({ auth }) {
                 </h2>
                 <div className="mt-8 flex flex-wrap justify-center gap-4">
                     <div class="relative inline-flex  group">
-                        <div class="absolute transitiona-all duration-1000 opacity-70-inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+                        <div class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
                         <a
                             href="#pricing"
                             title="Check Passport Status"
