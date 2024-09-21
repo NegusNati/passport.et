d72664232e0798993f7881c4b52670acf194c915
Negus <natnaelbirhanu22@gmail.com>
2024-09-21 09:58:39 +0300

    logo smaller on phones w-20

--- src/resources/js/Pages/Welcome.jsx
+++ src/resources/js/Pages/Welcome.jsx
@@ -38,7 +38,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                 <div className="relative min-h-screen pt-4 px-1 pt-50 selection:bg-[#FF2D20] selection:text-white sm:px-2 lg:px-4">
                     {/* <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl"> */}
                     <header className="flex flex-wrap justify-between items-center gap-2 px-1 py-2 lg:px-8">
-                        <div className="mr-auto pt-2 w-32 sm:w-auto">
+                        <div className="mr-auto pt-2 w-20 sm:w-auto">
                             <ApplicationLogo />
                         </div>
                         <nav className="ml-auto flex justify-between space-x-2 lg:space-x-4 ">
