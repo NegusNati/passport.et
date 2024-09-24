3fee500bc9c61b1d55833d15c79f34c0cf6f5f52
Negus <natnaelbirhanu22@gmail.com>
2024-09-24 10:50:33 +0300

    uncommnet stuff

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -2,4 +2,5 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = 'https://passport.et';
+// TODO; uncomment this when you merge
+// window.axios.defaults.baseURL = 'https://passport.et';
