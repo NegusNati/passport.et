63b4ff3a7ce2b52f422ccc7eb89f363646e3775c
Negus <natnaelbirhanu22@gmail.com>
2024-09-25 10:19:26 +0300

    sitemap url added

--- src/app/Console/Commands/GenerateSitemap.php
+++ src/app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -30,6 +31,20 @@ class GenerateSitemap extends Command
     {
         // modify this to your own needs
         SitemapGenerator::create(config('app.url'))
+            ->getSitemap()
+            ->add(Url::create('/'))
+            ->add(Url::create('/dashboard'))
+            ->add(Url::create('/passport'))
+            ->add(Url::create('/passport/search'))
+            ->add(Url::create('/passport'))
+            ->add(Url::create('/passport/{id}'))
+            ->add(Url::create('/all-passports'))
+            ->add(Url::create('/login'))
+            ->add(Url::create('/register'))
+            ->add(Url::create('/privacy'))
+            ->add(Url::create('/telegram'))
+            ->add(Url::create('/payment'))
+            ->add(Url::create('/subscribe'))
             ->writeToFile(public_path('sitemap.xml'));
     }
 }
