78b364015da83a0e0ffa34afde3a12f9b9a8d284
Negus <natnaelbirhanu22@gmail.com>
2024-09-30 11:26:32 +0300

    for prod

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -3,4 +3,4 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // TODO; uncomment this when you merge
-// window.axios.defaults.baseURL = 'https://passport.et';
+window.axios.defaults.baseURL = 'https://passport.et';
