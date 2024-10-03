7b424267e73790fa490b10e1611da15f32094dbc
Negus <natnaelbirhanu22@gmail.com>
2024-10-03 10:07:14 +0300

    cors and moved robot.txt to root

--- src/config/cors.php
+++ src/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [ env('APP_URL') , '*'],
+    'allowed_origins' => [env('APP_URL'), 'https://pagead2.googlesyndication.com', 'https://www.googletagmanager.com'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
