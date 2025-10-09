a43c4ce5eb21b9bd5b9eb98425953e51013ac629
Negus <natnaelbirhanu22@gmail.com>
2025-10-09 11:40:23 +0000

    fix build and robots.txt

---
+++ src/public/build/js/InputLabel-T8xBUO99.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-C4gz8KpY.js";function n({value:t,className:e="",children:r,...a}){return s.jsx("label",{...a,className:"block font-medium text-sm text-gray-700 dark:text-gray-300 "+e,children:t||r})}export{n as I};
