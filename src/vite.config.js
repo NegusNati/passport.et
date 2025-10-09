2005be47213149c2badbc80770aa83e7748e04de
negusnati <natnaelbirhanu22@gmail.com>
2025-10-09 16:15:34 +0300

    Refactor Traefik and Vite configurations for production environment
    - Updated `docker-compose.prod.yml` to configure Traefik for the API service, enhancing routing for `api.passport.et`.
    - Modified `vite.config.js` to enable HTTPS and set the correct host for hot module replacement, aligning with the new Traefik setup.
    - Adjusted `traefik.yml` to switch from TLS challenge to HTTP challenge for certificate issuance, improving compatibility with Let's Encrypt.
    
    These changes streamline the production environment and ensure secure API access.

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,31 +3,31 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        watch: {
-            usePolling: true,
-        },
-        hmr: {
-            host: "app.localhost",
-            protocol: "http",
-        },
-    },
-
-    // // TODO: uncommnet this on merge
     // server: {
     //     host: "0.0.0.0",
     //     port: 5173,
-    //     https: true,
     //     watch: {
     //         usePolling: true,
     //     },
     //     hmr: {
-    //         host: "passport.et",
-    //         protocol: "https",
+    //         host: "app.localhost",
+    //         protocol: "http",
     //     },
     // },
+
+    // // TODO: uncommnet this on merge
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        https: true,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            host: "api.passport.et",
+            protocol: "https",
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.jsx"],
